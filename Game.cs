3d687fb6d577ffbb425bd6d6b1621d8beacfb356
Nazar Melnik <115361939+Arlikey@users.noreply.github.com>
2024-10-03 12:02:18 +0300

    added libraries files

---
+++ Game.cs
@@ -0,0 +1,28 @@
+﻿namespace GameStudioClasses
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Developer { get; set; }
+        public Genre Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+
+    public enum Genre
+    {
+        Action,
+        Adventure,
+        RolePlaying,
+        Strategy,
+        Simulation,
+        Sports,
+        Puzzle,
+        Horror,
+        Racing,
+        Fighting,
+        Shooter,
+        Platformer,
+        Sandbox
+    }
+}
---
+++ GameStudioDbContext.cs
@@ -0,0 +1,23 @@
+﻿using GameStudioClasses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace GameStudioDbContextLibrary
+{
+    public class GameStudioDbContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+        public GameStudioDbContext() => Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
